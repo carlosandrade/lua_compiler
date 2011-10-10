046e5363c2f04025cced1b689d506f66984ff5d9
Carlos Andrade <carlosviansi@gmail.com>
2011-10-10 00:07:29 -0300

    criando metodos de verificacao de starter e adicionando outros parsers de producoes

--- Parser.java
+++ Parser.java
@@ -1,11 +1,72 @@
-//chunk::= (stat (';'|&) )* (laststat (';'|&)|&)
+/*
+Verifica se um dado token pertence aos starters de um não terminal
+*/
+
+//--> s(explist) = s(exp) = {nil, false, true, Number, String, '...', function, Name, '(', '{', '-', not, '#'}
+private boolean isExplistStarter(Token currentToken) {return isExpStarter()}
+
+
+//--> s(exp) = {nil, false, true, Number, String, '...', function, Name, '(', '{', '-', not, '#'}
+private boolean isExpStarter(Token currentToken)
+{
+    if((currentToken.kind == Token.NIL) || (currentToken.kind == Token.FALSE)
+    || (currentToken.kind == Token.TRUE) || (currentToken.kind == Token.NUMBER)
+    || (currentToken.kind == Token.STRING) ||(currentToken.kind == Token.THREEDOTES)
+    || (currentToken.kind == Token.FUNCTION) || (currentToken.kind == Token.NAME)
+    || (currentToken.kind == Token.LPAREN) || (currentToken.kind == Token.LCURLY)
+    || (currentToken.kind == Token.MINUS) || (currentToken.kind == Token.NOT)
+    || (currentToken.kind == Token.VELHA))
+    {return true}
+    else return false
+    
+}
+
+
+//--> s(laststat) ={return,break}
+private boolean isLaststatStarter(Token currentToken)
+{
+    if((currentToken.kind == Token.RETURN) || (currentToken.kind == Token.BREAK))
+    {return true}
+    else return false
+}
+
 //--> s(stat) = {Name,'(', do, while, repeat, if, for, function,local}
+private boolean isStatStarter(Token currentToken)
+{
+    if((currentToken.kind == Token.NAME) || (currentToken.kind == Token.LPAREN)
+     || (currentToken.kind == Token.DO) || (currentToken.kind == Token.WHILE)
+     || (currentToken.kind == Token.REPEAT) || (currentToken.kind == Token.IF)
+     || (currentToken.kind == Token.IF) || (currentToken.kind == Token.FOR)
+     || (currentToken.kind == Token.FUNCTION) || (currentToken.kind == Token.LOCAL))
+     {return true}
+     else return false
+}
+
+//--> s(prefixexp1): {':', s(args), &} = {':','(','{', String,&}
+//Se cadeia vazia refletir em true dentro do metodo a condicional
+//no codigo nao funciona! No parserX() que ele deve ser refletido como Dummy. Verificar isso.
+private boolean isStarterPrefixexp1(Token currentToken)
+{
+    if((currentToken.kind == Token.COLON) || (currentToken.kind == Token.LPAREN)
+     || (currentToken.kind == Token.LCURLY) || (currentToken.kind == Token.STRING))
+     {return true}
+     else return false
+}
+//--> s(prefixexp2):  { '[', '.', &}
+private boolean isStarterPrefixexp2(Token currentToken)
+{
+    if((currentToken.kind == Token.LBRACKET) || (currentToken.kind == Token.DOT))
+     {return true}
+     else return false
+}
+
+
+
+/*****************************************************************/
+
+//chunk::= (stat (';'|&) )* (laststat (';'|&)|&)
 private void parseChunck(){
-    while((currentToken.kind == Token.NAME) || (currentToken.kind == Token.LPAREN)
-    || (currentToken.kind == Token.DO) || (currentToken.kind == Token.WHILE)
-    || (currentToken.kind == Token.REPEAT) || (currentToken.kind == Token.IF)
-    || (currentToken.kind == Token.IF) || (currentToken.kind == Token.FOR)
-    || (currentToken.kind == Token.FUNCTION) || (currentToken.kind == Token.LOCAL))
+    while(isStatStarter(currentToken))
     {
         parseStat();
         if(currentToken.kind == Token.SEMICOLON)
@@ -13,7 +74,7 @@ private void parseChunck(){
         else
             dummy();
     }
-    if((currentToken.kind == Token.RETURN) || (currentToken.kind == Token.BREAK))
+    if(isLaststatStarter(currentToken))
     {
         parseLaststat();
         if(currentToken.kind == Token.SEMICOLON)
@@ -28,12 +89,98 @@ private void parseChunck(){
 }
 
 
+//laststat::= return (explist|&) | break
+private void parseLaststat(){
+    switch(currentToken.kind)
+    {
+        case Token.RETURN:
+        {
+            acceptIt();
+            if(isExplistStarter(currentToken))
+                parseExplist();
+            else
+                dummy();
+        }
+        case Token.BREAK:
+        {
+            acceptIt();
+        }
+    }
+}
 
+//funcname ::= Name (`.` Name)* ((`:` Name)|&)
+private void parseFuncname(){
+    accept(Token.NAME);
+    while(currentToken.kind == Token.DOT)
+    {
+        acceptIt();
+        accept(Token.NAME);
+    }
+    if(currentToken.kind == Token.COLON)
+    {
+        acceptIt();
+        accept(Token.NAME);
+    }
+    else
+        dummy();    
+}
 
+//var::= Name (prefixexp1 prefixexp2(`[` exp `]` |  `.` Name)| &) | '(' exp ')'  prefixexp1 prefixexp2 (`[` exp `]` |  `.` Name)
 
-
-
-
+private void parseVar()
+{
+    switch(currentToken.kind)
+    {
+        case Token.NAME:
+        {
+            acceptIt();
+            if(isStarterPrefixexp1() || isStarterPrefixexp2() || currentToken.kind == Token.LBRACKET)
+            {
+                parsePrefixexp1();
+                parsePrefixexp2();
+                switch(currentToken.kind)
+                {
+                    case Token.LBRACKET:
+                    {
+                        acceptIt();
+                        parseExp();
+                        accept(Token.RBRACKET);
+                    }
+                    case Token.DOT:
+                    {
+                        acceptIt();
+                        parseName();
+                    }
+                }
+            }
+            else
+                dummy();
+        }
+        case Token.LPAREN:
+        {
+            acceptIt();
+            parseExp();
+            accept(Token.RPAREN);
+            parsePrefixexp1();
+            parsePrefixexp2();
+            switch(currentToken.kind)
+            {
+                case Token.LBRACKET:
+                {
+                    acceptIt();
+                    parseExp();
+                    accept(Token.RBRACKET);
+                }
+                case Token.DOT:
+                {
+                    acceptIt();
+                    parseName();
+                }
+            }
+        }
+    }
+    
+}
 
 
 
